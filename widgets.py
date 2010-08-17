d41f5c591325106cd56ca24f375f28a463944532
Matt Austin <matt@busara.com.au>
2010-08-17 17:03:18 +0800

    Removed unnecessary <ul></ul> from 'selected_images' placeholder in ImageSelectMultiple widget.

--- widgets.py
+++ widgets.py
@@ -31,7 +31,7 @@ class ImageSelectMultiple(SelectMultiple):
         image_picker_url = reverse('media_image_picker', args={})
         output += mark_safe(
             '<a class="action initiate_image_picker" href="%s">select images</a>\
-                <div class="selected_images"><ul></ul></div>'
+                <div class="selected_images"></div>'
             %(image_picker_url))
         return output
 
