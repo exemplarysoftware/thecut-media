5bcffc522e627f5df21308aec8c6aa030f11ed8f
Matt Austin <matt.austin@thecut.net.au>
2011-03-10 08:34:26 +0800

    Removed files from old media app. Updated requirements in README / setup.py.

--- thecut/media/signals.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-
-def set_publish_at(sender, instance, **kwargs):
-    """If not already set, automatically set the instance's publish_at value to now."""
-    instance.publish_at = instance.publish_at or datetime.now()
-
