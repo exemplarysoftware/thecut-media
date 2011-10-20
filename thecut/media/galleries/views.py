68fa9c9a77e93de8ce88f82a87e3aa1c1c4748a6
Matt Austin <matt.austin@thecut.net.au>
2011-10-20 11:48:54 +0800

    Sitemap refactor. Included paginated gallery list urls in sitemap.

--- thecut/media/galleries/views.py
+++ thecut/media/galleries/views.py
@@ -3,11 +3,8 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.list_detail import object_detail, object_list
+from thecut.media.galleries import settings
 from thecut.media.galleries.models import Gallery
-
-
-PAGINATE_BY = getattr(settings, 'GALLERIES_PAGINATE_BY', 10)
-MEDIA_PAGINATE_BY = getattr(settings, 'GALLERIES_MEDIA_PAGINATE_BY', 10)
 
 
 def gallery_list(request, queryset=None, **kwargs):
@@ -16,7 +13,7 @@ def gallery_list(request, queryset=None, **kwargs):
     
     if kwargs.get('page', None) == '1':
         return redirect(reverse('galleries:gallery_list'))
-    kwdefaults = {'paginate_by': PAGINATE_BY, 'page': 1,
+    kwdefaults = {'paginate_by': settings.GALLERY_PAGINATE_BY, 'page': 1,
         'template_object_name': 'gallery'}
     kwdefaults.update(kwargs)
     
@@ -49,7 +46,7 @@ def gallery_media_list(request, slug, queryset=None, **kwargs):
             kwargs={'slug': gallery.slug}))
     
     kwargs.update({'extra_context': {'gallery': gallery}})
-    kwdefaults = {'paginate_by': MEDIA_PAGINATE_BY, 'page': 1,
+    kwdefaults = {'paginate_by': settings.GALLERY_MEDIA_PAGINATE_BY, 'page': 1,
         'template_name': 'galleries/gallery_media_list.html',
         'template_object_name': 'gallery_media'}
     if 'facebook' in settings.INSTALLED_APPS:
