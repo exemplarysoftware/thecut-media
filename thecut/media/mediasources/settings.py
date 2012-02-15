40f522fdde233b4247fe53c9dcb8bb51b641112a
Matt Austin <matt.austin@thecut.net.au>
2012-02-15 11:35:04 +0800

    Updated thumbnail settings so that admin thumbnails are processed first.

--- thecut/media/mediasources/settings.py
+++ thecut/media/mediasources/settings.py
@@ -9,13 +9,13 @@ CELERY = 'djcelery' in settings.INSTALLED_APPS
 ADMIN_IMAGE_THUMBNAILS = getattr(settings,
     'MEDIASOURCES_ADMIN_IMAGE_THUMBNAILS', [
     # List of tuples containing geometry_size and options dict
-    ('140x106', {'crop': 'center'}),
-    ('100x75', {'crop': 'center'}),
     ('60x45', {'crop': 'center'}),
+    ('100x75', {'crop': 'center'}),
+    ('140x106', {'crop': 'center'}),
 ])
 
-IMAGE_THUMBNAILS = getattr(settings,
-    'MEDIASOURCES_IMAGE_THUMBNAILS', []) + ADMIN_IMAGE_THUMBNAILS
+IMAGE_THUMBNAILS = ADMIN_IMAGE_THUMBNAILS + getattr(settings,
+    'MEDIASOURCES_IMAGE_THUMBNAILS', [])
 
 
 DOCUMENT_THUMBNAILS = getattr(settings,
@@ -25,17 +25,17 @@ DOCUMENT_THUMBNAILS = getattr(settings,
 ADMIN_VIDEO_THUMBNAILS = getattr(settings,
     'MEDIASOURCES_ADMIN_VIDEO_THUMBNAILS', [
     # List of tuples containing geometry_size and options dict
-    ('140x106', {'crop': 'center'}),
-    ('100x75', {'crop': 'center'}),
     ('60x45', {'crop': 'center'}),
+    ('100x75', {'crop': 'center'}),
+    ('140x106', {'crop': 'center'}),
 ])
 
-VIDEO_THUMBNAILS = getattr(settings,
-    'MEDIASOURCES_VIDEO_THUMBNAILS', []) + ADMIN_VIDEO_THUMBNAILS
+VIDEO_THUMBNAILS = ADMIN_VIDEO_THUMBNAILS + getattr(settings,
+    'MEDIASOURCES_VIDEO_THUMBNAILS', [])
 
-YOUTUBE_VIDEO_THUMBNAILS = getattr(settings,
-    'MEDIASOURCES_YOUTUBE_VIDEO_THUMBNAILS', []) + ADMIN_VIDEO_THUMBNAILS
+YOUTUBE_VIDEO_THUMBNAILS = ADMIN_VIDEO_THUMBNAILS + getattr(settings,
+    'MEDIASOURCES_YOUTUBE_VIDEO_THUMBNAILS', [])
 
-VIMEO_VIDEO_THUMBNAILS = getattr(settings,
-    'MEDIASOURCES_VIMEO_VIDEO_THUMBNAILS', []) + ADMIN_VIDEO_THUMBNAILS
+VIMEO_VIDEO_THUMBNAILS = ADMIN_VIDEO_THUMBNAILS + getattr(settings,
+    'MEDIASOURCES_VIMEO_VIDEO_THUMBNAILS', [])
 
