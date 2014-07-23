f012005a26005b2a669bc0a27462a762108f3d00
Josh Crompton <josh.crompton@thecut.net.au>
2014-07-23 09:28:11 +0800

    Make django-taggit tags for all existing django-tagging tags.
    Refs #3511.
    
    - Add django-taggit to setup.py install_requires.
    - Add data migration for galleries models with django-tagging tags.
    - Add data migration for media sources with django-tagging tags.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=get_git_version(),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['django-model-utils>=1.2.0',
-                      'django-tagging>=0.3.1', 'python-magic>=0.4.6',
+    install_requires=['django-model-utils>=1.2.0', 'django-tagging>=0.3.1',
+                      'django-taggit==0.12', 'python-magic>=0.4.6',
                       'pillow>=2.0.0', 'sorl-thumbnail>=11.12,<12'],
 )
