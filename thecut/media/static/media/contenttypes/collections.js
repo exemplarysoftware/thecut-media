bde4b225348d090073c294041122ca61b346fa5d
Matt Austin <matt.austin@thecut.net.au>
2014-07-25 14:16:19 +0800

    Abstracted some of the functionality for django rest framework paginated responses.

--- thecut/media/static/media/contenttypes/collections.js
+++ thecut/media/static/media/contenttypes/collections.js
@@ -1,39 +1,13 @@
-define(['backbone.paginator', 'contenttypes/models'], function(PageableCollection, models) {
+define(['djangorestframework', 'contenttypes/models'], function(djangorestframework, models) {
 
 
-    var ContentTypeCollection = PageableCollection.extend({
+    var ContentTypeCollection = djangorestframework.PageableCollection.extend({
 
-        mode: 'server',
         model: models.ContentType,
 
         initialize: function(models, options) {
             this.url = options['url'];
         },
-
-        queryParams: {
-            currentPage: 'page',
-            pageSize: 'limit',
-            totalPages: null,
-            totalRecords: null,
-            sortKey: null,
-            order: null
-        },
-
-        state: {
-            pageSize: null
-        },
-
-        parseLinks: function (data) {
-            return {next: data.next, prev: data.previous}
-        },
-
-        parseRecords: function (data) {
-            return data.results;
-        },
-
-        parseState: function (data) {
-            return {totalRecords: data.count}
-        }
 
     });
 
---
+++ thecut/media/static/media/djangorestframework.js
@@ -0,0 +1,41 @@
+define(['backbone.paginator'], function(BasePageableCollection) {
+
+
+    var PageableCollection = BasePageableCollection.extend({
+
+        mode: 'server',
+
+        queryParams: {
+            currentPage: 'page',
+            pageSize: 'limit',
+            totalPages: null,
+            totalRecords: null,
+            sortKey: null,
+            order: null
+        },
+
+        state: {
+            pageSize: null
+        },
+
+        parseLinks: function (data) {
+            return {next: data.next, prev: data.previous}
+        },
+
+        parseRecords: function (data) {
+            return data.results;
+        },
+
+        parseState: function (data) {
+            return {totalRecords: data.count}
+        }
+
+    });
+
+
+    return {
+        'PageableCollection': PageableCollection
+    };
+
+
+});
