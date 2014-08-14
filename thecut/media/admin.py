b1a73a8621bca2adf14b84bf802ab08fc9337f88
Matt Austin <matt.austin@thecut.net.au>
2014-08-14 11:03:50 +0800

    Removal of old admin media picker forms/views/templates/urls.

--- thecut/media/admin.py
+++ thecut/media/admin.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from . import views
 from .forms import AttachedMediaItemInlineForm
 from .models import AttachedMediaItem
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, patterns
 from django.contrib import admin
 from django.contrib.contenttypes.generic import GenericStackedInline
 
@@ -32,14 +31,6 @@ class AttachedMediaItemMixin(admin.ModelAdmin):
     def get_urls(self):
         urlpatterns = patterns(
             'thecut.media.views',
-            url(r'^(?:\d+|add)/media/contenttype/$',
-                views.AdminContentTypeList.as_view()),
-            url(r'^(?:\d+|add)/media/contenttype/(?P<content_type_pk>\d+)'
-                '/picker$',
-                views.AdminContentTypeObjectList.as_view()),
-            url(r'^(?:\d+|add)/media/contenttype/(?P<content_type_pk>\d+)'
-                '/list$',
-                views.AdminContentTypeSelectedObjectList.as_view()),
             (r'^media/api/', include('thecut.media.api.urls')),
         )
         urlpatterns += super(AttachedMediaItemMixin, self).get_urls()
--- thecut/media/forms.py
+++ thecut/media/forms.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django import forms
-from django.contrib.contenttypes.models import ContentType
-from thecut.media import MEDIA_SOURCE_CLASSES
 from thecut.media.models import AttachedMediaItem
 
 
@@ -15,14 +13,3 @@ class AttachedMediaItemInlineForm(forms.ModelForm):
     class Meta(object):
         fields = ['order', 'content_type', 'object_id']
         model = AttachedMediaItem
-
-
-class MediaSearchForm(forms.Form):
-
-    q = forms.CharField(required=False)
-    tags = forms.MultipleChoiceField(required=False, choices=[],
-                                     widget=forms.CheckboxSelectMultiple())
-
-    def __init__(self, tag_list, *args, **kwargs):
-        super(MediaSearchForm, self).__init__(*args, **kwargs)
-        self.fields['tags'].choices = [(t.pk, t.name) for t in tag_list]
