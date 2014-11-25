aed4ab3199a982d9e08e855177bf281aa2317913
Matt Austin <matt.austin@thecut.net.au>
2014-11-25 17:12:52 +0800

    Import GenericForeignKey from old location for pre django 1.7 compatibility.

--- thecut/media/fields.py
+++ thecut/media/fields.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+try:
+    from django.contrib.contenttypes.fields import GenericForeignKey
+except ImportError:  # pre Django 1.7 compatibility
+    from django.contrib.contenttypes.generic import GenericForeignKey
 
 
 class MediaForeignKey(GenericForeignKey):
