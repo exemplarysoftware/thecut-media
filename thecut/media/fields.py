8a65a9f65f5382d644ca316251d31035bb8345e1
Matt Austin <matt.austin@thecut.net.au>
2015-09-17 08:45:36 +0800

    Override ``MediaForeignKey`` ``get_content_type`` method to return MediaContentType instance. Allows easier setting of ``content_object`` without Django complaining.

--- thecut/media/fields.py
+++ thecut/media/fields.py
@@ -13,3 +13,9 @@ class MediaForeignKey(GenericForeignKey):
     def _check_content_type_field(self):
         # TODO Custom check for MediaContentType?
         return []
+
+    def get_content_type(self, *args, **kwargs):
+        from .models import MediaContentType
+        content_type = super(MediaForeignKey, self).get_content_type(*args,
+                                                                     **kwargs)
+        return MediaContentType.objects.get(pk=content_type.pk)
