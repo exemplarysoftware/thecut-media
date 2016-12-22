8ce2db84b5bfd6b59daa9de6ded4be5b6f386336
Mark Lockett <mark.lockett@thecut.net.au>
2016-12-22 11:39:09 +0800

    Add unit test for the add_media_generic_relation receiver Refs #9326

---
+++ thecut/media/tests/test_receivers.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from thecut.publishing.models import Content
+from django.db import models
+from thecut.media import receivers
+
+
+class TestAddMediaGenericRelection(TestCase):
+    def test_receiver(self):
+        models.signals.class_prepared.disconnect(
+            receivers.add_media_generic_relation)
+        class TestContent(Content):
+            pass
+        models.signals.class_prepared.connect(
+            receivers.add_media_generic_relation)
+        self.assertFalse(hasattr(TestContent, 'media'))
+        receivers.add_media_generic_relation(TestContent)
+        self.assertTrue(hasattr(TestContent, 'media'))
