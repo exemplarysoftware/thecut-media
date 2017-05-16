281cadf580bea34cf24eca855b47d16519ed591e
Matt Austin <devops@mattaustin.com.au>
2017-05-16 13:38:27 +0800

    Import reduce from functools (python 3 compatibility).

--- thecut/media/managers.py
+++ thecut/media/managers.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from .utils import get_media_source_models
 from django.contrib.contenttypes.models import ContentTypeManager
 from django.db.models import Manager, Model, Q
+import functools
 import operator
 
 
@@ -32,9 +33,10 @@ class MediaContentTypeManager(ContentTypeManager):
                 q_args += [(app_label, model_name)]
             elif issubclass(model, Model):
                 q_args += [(model._meta.app_label, model._meta.model_name)]
-        query = reduce(operator.or_, (Q(app_label__iexact=app_label,
-                                        model__iexact=model)
-                                      for app_label, model in q_args))
+        query = functools.reduce(operator.or_,
+                                 (Q(app_label__iexact=app_label,
+                                    model__iexact=model)
+                                  for app_label, model in q_args))
         return self.filter(query)
 
     def get_queryset(self, *args, **kwargs):
@@ -44,9 +46,10 @@ class MediaContentTypeManager(ContentTypeManager):
         # Evaluate the queryset and store it on the class
         if MediaContentTypeManager._media_queryset is None:
             models = get_media_source_models()
-            query = reduce(operator.or_, (Q(app_label=model._meta.app_label,
-                                            model=model._meta.model_name)
-                                          for model in models))
+            query = functools.reduce(
+                operator.or_, (Q(app_label=model._meta.app_label,
+                                 model=model._meta.model_name)
+                               for model in models))
             queryset = queryset.filter(query)
             MediaContentTypeManager._media_queryset = queryset
 
