8eea15a314a3314b4831f0d15934224e801786d9
Matt Austin <devops@mattaustin.com.au>
2017-05-23 16:27:03 +0800

    Update media management command for new django compatibility. Note: findthumbnails util method has not yet been updated.

--- thecut/media/management/commands/media.py
+++ thecut/media/management/commands/media.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.core.management.base import BaseCommand, CommandError
-from optparse import make_option
 
 
 class Command(BaseCommand):
 
-    args = '[findthumbnails] [queuethumbnails]'
-
-    option_list = BaseCommand.option_list + (
-        make_option('--quantity', action='store', type='int', dest='quantity',
-                    default=1, help='Number of instances to create'),
-    )
-
     help = ('findthumbnails: Attempt to find thumbnail references in '
             'templates\n'
             'queuethumbnails: Queue thumbnail generation for media instances.')
+
+    def add_arguments(self, parser):
+        parser.add_argument('command')
 
     def _find_thumbnails(self):
         from thecut.media.utils import find_thumbnails_in_templates
@@ -33,10 +28,12 @@ class Command(BaseCommand):
         except AttributeError:
             print(output)
 
-    def handle(self, command, *args, **options):
+    def handle(self, *args, **options):
+
+        command = options.get('command')
 
         if command not in ['findthumbnails', 'queuethumbnails']:
-            raise CommandError('"{0}" is not a valid argument'.format(command))
+            raise CommandError('"{}" is not a valid argument'.format(command))
 
         if command == 'findthumbnails':
             return self._find_thumbnails()
