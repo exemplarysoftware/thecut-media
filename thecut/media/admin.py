5a3781390f57ddda63f5e1bb33c538459a5376b5
Mark Lockett <mark.lockett@thecut.net.au>
2017-05-23 14:31:09 +0800

    Correct set the api endpoint on an add view Refs #9373

--- thecut/media/admin.py
+++ thecut/media/admin.py
@@ -58,3 +58,10 @@ class AttachedMediaItemMixin(admin.ModelAdmin):
         extra_context['data_api_href'] = reverse(self.admin_site.name + ":" + media_api_namespace + ":contenttype_list")
         return super(AttachedMediaItemMixin, self).change_view(request, object_id, form_url, extra_context)
 
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        media_api_namespace = 'media_api-{0}-{1}'.format(
+            self.model._meta.app_label, self.model._meta.model_name)
+        extra_context['data_api_href'] = reverse(self.admin_site.name + ":" + media_api_namespace + ":contenttype_list")
+        return super(AttachedMediaItemMixin, self).add_view(request, form_url, extra_context)
+
