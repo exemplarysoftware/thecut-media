388eea489e439d1d8190c77510b1456eb3df2f9a
Matt Austin <matt.austin@thecut.net.au>
2017-07-26 14:04:42 +0800

    Connect media receivers in app ``ready`` rather than ``__init__``. Refs #5133.

--- thecut/media/apps.py
+++ thecut/media/apps.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from . import receivers
 from django import apps
+from django.db.models import signals
 
 
 class AppConfig(apps.AppConfig):
 
     name = 'thecut.media'
 
-    def __init__(self, *args, **kwargs):
-        from . import receivers
-        from django.db.models import signals
-        signals.pre_delete.connect(receivers.delete_media_attachments)
-        # Let's be helpful, and add media generic relation field to anything
+    def ready(self):
+        signals.pre_delete.connect(
+            receivers.delete_media_attachments,
+            dispatch_uid='thecut.media.delete_media_attachments')
+        # Let's be "helpful", and add media generic relation field to anything
         # that extends the Content model.
-        signals.pre_init.connect(receivers.add_media_generic_relation)
-        return super(AppConfig, self).__init__(*args, **kwargs)
+        signals.pre_init.connect(
+            receivers.add_media_generic_relation,
+            dispatch_uid='thecut.media.add_media_generic_relation')
